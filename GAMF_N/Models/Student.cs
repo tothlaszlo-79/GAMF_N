6d8b3a13c5bda7d0d9c018436045a58c41462428
Toth Laszlo <lastoth@gmail.com>
2022-10-06 19:01:27 +0200

    2 óra anyaga

--- GAMF_N/Models/Student.cs
+++ GAMF_N/Models/Student.cs
@@ -1,12 +1,24 @@
-﻿namespace GAMF_N.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GAMF_N.Models
 {
     public class Student
     {
         public int Id { get; set; }
+
+        [Display(Name = "Családnév")]
+        [Required(ErrorMessage ="Családnév itötése kötlező!")]
         public string LastName { get; set; }
+
+        [Display(Name = "Keresztnév")]
+        [Required(ErrorMessage ="Keresztnév kitöltése kötelező!")]
         public string FirstMidName { get; set; }
+
+        [Display(Name ="Első jelentkezés")]
+        [Required(ErrorMessage ="Első jelentkezés kitöltése kötelező!")]
         public DateTimeOffset EnrollmentDate { get; set; }
 
+        [Display(Name ="Jelentkezések")]
         public virtual ICollection<Enrollment> Enrollments { get; set; }
 
     }
