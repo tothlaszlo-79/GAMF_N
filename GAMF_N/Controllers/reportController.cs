ab73e8096d9f1fa5178566256d19edcba5edba13
Toth Laszlo <lastoth@gmail.com>
2022-10-13 19:01:03 +0200

    Harmadik rész

---
+++ GAMF_N/Controllers/reportController.cs
@@ -0,0 +1,29 @@
+﻿using GAMF_N.Data;
+using GAMF_N.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GAMF_N.Controllers
+{
+    public class reportController : Controller
+    {
+        private readonly GAMFDbContext _Context;
+
+        public reportController(GAMFDbContext context)
+        {
+            _Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IActionResult EnrollmentDateReport()
+        {
+            var result = _Context.Students.GroupBy(s => s.EnrollmentDate)
+                .Select(s => new EnrollmentDateVM
+                {
+                    EnrollmentDate = s.Key,
+                    StudentCount = s.Count()
+                });
+
+
+            return View(result.ToList());
+        }
+    }
+}
---
+++ GAMF_N/Models/EnrollmentDateVM.cs
@@ -0,0 +1,8 @@
+﻿namespace GAMF_N.Models
+{
+    public class EnrollmentDateVM
+    {
+        public DateTimeOffset EnrollmentDate { get; set; }
+        public int StudentCount { get; set; }
+    }
+}
